0c722bd0ded77ec3d478a2b27e1dfc0ba568a92d
Nikita Kumari <1018nikitakumari@gmail.com>
2023-04-11 14:34:24 +0530

    Add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,7 +35,9 @@ export class AppComponent {
       }
     });
     
+    // This line toggles the visibility of the side navigation bar
     this.showSideNav = false;
+    // This block of code subscribes to router events and checks the current URL to determine whether to show or hide the side navigation bar
     this.router.events.subscribe((val) => {
       if (this.router.url === '/' || this.router.url === '/login-signup/register' || this.router.url === '/login-signup/login') {
         this.showSideNav = false;
@@ -45,14 +47,21 @@ export class AppComponent {
     });    
   }
 
+  //lifecycle hook that is called when the component is initialized
   ngOnInit(): void { 
   }
 
+  /**
+   * This method is called when the side navigation bar is toggled
+   * Set the screen width to the data passed in
+   * Set the isSideNavCollapsed property to the data passed in
+   */
   onToggleSidenav(data: SideNavToggle): void {
     this.screenWidth =data.screenWidth;
     this.isSideNavCollapsed = data.collapsed;  
   }
 
+  // This method checks whether the current URL starts with the given path
   urlStartsWith(path : string) : boolean {
     return this.router.url.startsWith(path);
   }   
